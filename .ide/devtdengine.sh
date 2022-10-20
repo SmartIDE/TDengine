43b44077ef999a5da5812ef00c4a362f3d70986e
netcorevip <netcorevip@outlook.com>
2022-10-20 14:58:07 +0000

    配置优化

--- .ide/devtdengine.sh
+++ .ide/devtdengine.sh
@@ -16,9 +16,12 @@ cp /home/project/.ide/taos.cfg /home/project/sim/cfg/
 cp /home/project/.ide/launch.json /home/project/.vscode/
 
 echo "------------下载插件"
+cd .ide
 wget https://github.com/SmartIDE/cppvsix/releases/download/v1.0.0/ms-vscode.cmake-tools-1.13.15.vsix
 wget https://github.com/SmartIDE/cppvsix/releases/download/v1.0.0/ms-vscode.cpptools-1.13.2@linux-x64.vsix
 
 echo "------------安装插件"
 code --install-extension /home/project/.ide/ms-vscode.cmake-tools-1.13.15.vsix
 code --install-extension /home/project/.ide/ms-vscode.cpptools-1.13.2@linux-x64.vsix
+
+cd ../
